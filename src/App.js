fc2db9142c20540325cdb56fb0d2faed510d1a51
gloriafercu <gloriafercu@gmail.com>
2018-03-10 13:39:37 +0100

    print pokemons and filter by name

--- src/App.js
+++ src/App.js
@@ -5,122 +5,79 @@ import './main.css';
 class App extends React.Component {
 	constructor(props) {
 		super(props);
+		this.handleOnChange = this.handleOnChange.bind(this);
 		this.state = {
 			pokemonArray: [],
-			pokemonId: [],
-			pokemonImg: [],
-			pokemonName: [],
-			pokemonType: []
+			valueInput: ''
 		};
 	}
 
-	const paco = (id ) => {
+
+	componentDidMount() {
 		let pokemonArray = [];
+		for (let i=1; i<= 3 ; i++) {
+			fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`)
+			.then(response => response.json())
+			.then(json => {
+				const pokemonObject = json;
+				pokemonArray.push(pokemonObject);
+				console.log('pokemonObject: ', pokemonObject);
+				this.setState({
+					pokemonArray: pokemonArray
+				});
+			});
+		};
+		console.log('pokemonArray: ', pokemonArray);
+	}
 
-	  for (let i = 1; i <= 3; i++) {
-	    let url = `https://pokeapi.co/api/v2/pokemon/${id}`
-
-	    fetch(url)
-	      .then(res => res.json())
-	      .then(json =>
-					const pokemonObject = json;
-					pokemonArray.push(pokemonObject);
-					console.log('Este es' + pokemonObject);
-
-					this.setState({
-						pokemonArray: pokemonArray,
-						pokemonId: json.id,
-						pokemonImg: json.sprites.front_default,
-						pokemonName: json.name,
-						pokemonType: json.types[0].type.name
-					});
-				};
-			}
-
-
-
-// 	getPokemons(i) {
-// 		fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`)
-// 		.then(response => response.json())
-// 		.then(json => {
-// 			let pokemonArray = [];
-// 			for (let i=1; i<= 3 ; i++) {
-// 			const pokemonObject = json;
-// 			pokemonArray.push(pokemonObject);
-// 			console.log('Este es' + pokemonObject);
-//
-// 			this.setState({
-// 				pokemonArray: pokemonArray,
-// 				pokemonId: json.id,
-// 				pokemonImg: json.sprites.front_default,
-// 				pokemonName: json.name,
-// 				pokemonType: json.types[0].type.name
-// 			});
-// 		});
-// 	};
-// }
-
-
-	// componentDidMount() {
-	// 	let pokemonArray = [];
-	// 	for (let i=1; i<= 3 ; i++) {
-	// 		fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`)
-	// 		.then(response => response.json())
-	// 		.then(json => {
-	//
-	// 			const pokemonObject = json;
-	// 			pokemonArray.push(pokemonObject);
-	// 			console.log(pokemonArray);
-	//
-	//
-	//
-	// 			this.setState({
-	// 				pokemonArray: pokemonArray,
-	// 				pokemonId: json.id,
-	// 				pokemonImg: json.sprites.front_default,
-	// 				pokemonName: json.name,
-	// 				pokemonType: json.types[0].type.name
-	// 			});
-	// 		});
-	// 	};
-	// }
-
-
-
-
+	handleOnChange(e) {
+		const valueToShow = e.target.value;
+		this.setState({
+			valueInput: valueToShow.toLowerCase()
+		});
+	}
 
 	printPokemons() {
+		const listPokemons = this.state.pokemonArray.filter(item =>
+      item.name.toLowerCase().includes(this.state.valueInput)
+    );;
+
 		return (
 			<ul className="pokemon__list">
-
-				<li className="pokemon__list--li">
-					<div className="pokemon__card">
-						<p className="pokemon__name">{this.state.pokemonName}</p>
-						<p className="pokemon__id">{this.state.pokemonId}</p>
-						<img src={this.state.pokemonImg} alt="pokemon"/>
-						<p className="pokemon__type">{this.state.pokemonType}</p>
-					</div>
-				</li>
+				{ listPokemons.map((list, index) =>
+					<li className="pokemon__list--li" key = {index}>
+						<div className="pokemon__card">
+							<p className="pokemon__name">{list.name}</p>
+							<p className="pokemon__id">{list.id}</p>
+							<img src={list.sprites.front_default} alt="pokemon"/>
+							<div className="pokemon__type">
+								{list.types.map((type, i) =>
+									<span key={i}> {list.types[i].type.name} </span>
+								)}
+							</div>
+						</div>
+					</li>)
+				}
 			</ul>
+
 		);
 	}
 
-
-
-
-
 	render() {
-		return (<div className="App">
-			<header className="App-header">
-				<h1 className="App-title">Pokémon Pokédex</h1>
-			</header>
-			<div className="input__search">
-				<input type="text" className="input__name" placeholder="Search pokémons by name"/>
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1 className="App-title">Pokémon Pokédex</h1>
+				</header>
+
+				<div className="input__search">
+					<input type="text" className="input__name" placeholder="Search pokémons by name" onChange = { this.handleOnChange }/>
+				</div>
+
+				{ this.printPokemons() }
+
 			</div>
-
-			{ this.printPokemons() }
-
-		</div>);
+		);
 	}
 }
 
