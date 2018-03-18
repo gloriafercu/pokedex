14d10e883754c91b93d3459d1d28ea29e6d93cbb
gloriafercu <gloriafercu@gmail.com>
2018-03-18 23:33:46 +0100

    add styles

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
 	}
 	componentDidMount() {
 		let totalPokemon = [];
-		for (let i=1; i<= 4; i++) {
+		for (let i=1; i<= 25; i++) {
 			fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`)
 			.then(response => response.json())
 			.then(details => {
--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -14,33 +14,47 @@ class PokemonDetails extends React.Component{
 						<Switch>
 							<Route exact path="/" component={App} />
 						</Switch>
-						<p className="pokemon__id">#{this.props.idDetails.toLocaleString('en-US', {minimumIntegerDigits: 2})}</p>
-						<div className="pictures">
-							<img className="picture front_default" src={this.props.imgFrontD} alt={this.props.nameDetails} />
-							<img className="picture back_default" src={this.props.imgBackD} alt={this.props.nameDetails} />
-							<img className="picture front_shiny" src={this.props.imgFrontS} alt={this.props.nameDetails} />
-							<img className="picture back_shiny" src={this.props.imgBackS} alt={this.props.nameDetails} />
-						</div>
-						<p className="pokemon__name">{this.props.nameDetails}</p>
-						<div className="pokemon__type">
-							{this.props.attackDetails.map((type, i) =>
-								<span className={`box box--${this.props.attackDetails[i].type.name.toLowerCase()}`} key={i}> {this.props.attackDetails[i].type.name} </span>
-							)}
-						</div>
-						<div className="pokemon__abilities">
-							<span className="abilities__title">Abilities:</span>
-							{this.props.skillsDetails.map((ability, i) =>
-								<span className="ability" key={i}>{this.props.skillsDetails[i].ability.name} </span>)}
-						</div>
-						<div className="pokemon__weight">
-							<span className="weight__title">Weight:</span>
-							<span className="height__detail">{this.props.weightDetails/10}</span>
-							<span className="height__kg">kg</span>
-						</div>
-						<div className="pokemon__height">
-							<span className="height__title">Height:</span>
-							<span className="height__detail">{this.props.heightDetails/10}</span>
-							<span className="height__m">m</span>
+
+						<div className="info">
+							<div className="cover">
+								<img className="front_default" src={this.props.imgFrontD} alt={this.props.nameDetails} />
+								<span className="details__id">#{this.props.idDetails.toLocaleString('en-US', {minimumIntegerDigits: 2})}</span>
+							</div>
+
+							<div className="picture">
+								<img className=" back_default" src={this.props.imgBackD} alt={this.props.nameDetails} />
+							</div>
+							<div className="picture">
+								<img className="front_shiny" src={this.props.imgFrontS} alt={this.props.nameDetails} />
+							</div>
+							<div className="picture">
+								<img className="back_shiny" src={this.props.imgBackS} alt={this.props.nameDetails} />
+							</div>
+							<div className="info__details">
+								<p className="pokemon__name">{this.props.nameDetails}</p>
+								<div className="pokemon__type">
+									{this.props.attackDetails.map((type, i) =>
+										<span className={`box box--${this.props.attackDetails[i].type.name.toLowerCase()}`} key={i}> {this.props.attackDetails[i].type.name} </span>
+									)}
+								</div>
+								<div className="info__card">
+									<div className="pokemon__abilities">
+										<span className="abilities__title">Abilities: </span>
+										{this.props.skillsDetails.map((ability, i) =>
+											<span className="ability" key={i}>{this.props.skillsDetails[i].ability.name} </span>)}
+									</div>
+									<div className="pokemon__weight">
+										<span className="weight__title">Weight: </span>
+										<span className="weight__detail">{this.props.weightDetails/10} kg</span>
+									</div>
+									<div className="pokemon__height">
+										<span className="height__title">Height: </span>
+										<span className="height__detail">
+											{this.props.heightDetails/10} m
+										</span>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
